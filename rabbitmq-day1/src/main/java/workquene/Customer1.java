c88e0658c98bda6228f687d42709ec330463e6ab
sixminutestogg <yangyang.zhang1@newtouch.com>
2020-09-04 17:15:44 +0800

    地方

---
+++ rabbitmq-day1/src/main/java/workquene/Customer1.java
@@ -0,0 +1,35 @@
+package workquene;
+
+import com.rabbitmq.client.*;
+import util.RabbitMqUtils;
+
+import java.io.IOException;
+
+/**
+ * @author a
+ */
+public class Customer1 {
+
+    public static void main(String[] args) {
+        try {
+
+            //获取链接
+            Connection connection = RabbitMqUtils.getConnection();
+            //获取通道
+            Channel channel = connection.createChannel();
+
+            //通道 队列绑定
+            channel.queueDeclare("work",true,false,false,null);
+
+            //接收消息
+            channel.basicConsume("work",true,new DefaultConsumer(channel){
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    System.out.println("消费者-1消费："+new String(body));
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ rabbitmq-day1/src/main/java/workquene/Provider.java
@@ -0,0 +1,38 @@
+package workquene;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+import util.RabbitMqUtils;
+
+/**
+ * @author Administrator
+ * 工作队列 的 生产者
+ */
+public class Provider {
+
+    public static void main(String[] args) {
+        try {
+
+            //获取对象
+            Connection connection = RabbitMqUtils.getConnection();
+            
+            //获取通道
+            Channel channel = connection.createChannel();
+
+            //通道绑定队列
+            channel.queueDeclare("work",true,false,false,null);
+
+            //发送消息
+            for (int i =0 ;i<=10 ; i++){
+
+                channel.basicPublish("","work", MessageProperties.PERSISTENT_TEXT_PLAIN,("第"+i+"次的 work queuen ! 的消息").getBytes());
+            }
+
+            RabbitMqUtils.closeChancleAndConnection(channel,connection);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
